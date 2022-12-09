f63dfe9b8f7a3449c3332a7b5d71fea5b1b72764
Jerry.chen <jerrychen.works@gmail.com>
2022-12-09 12:12:25 +0800

    New project

---
+++ Screener_mod.py
@@ -0,0 +1,20 @@
+
+class Screener:
+    """A Python stock screener"""
+
+    def __init__(self):
+        self.tickers = []
+
+    def low_ev(self, threshold):
+        """find stocks with negative enterprise value"""
+
+        screener_result = []
+
+        return screener_result
+
+    def low_lcr(self, threshold):
+        """find stocks with negative enterprise value"""
+
+        screener_result = []
+
+        return screener_result
