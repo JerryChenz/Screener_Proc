6cc7187c11f43444d2a1507fd8f68870768764f3
jerry <jerrychen.works@gmail.com>
2025-04-18 13:03:49 +0800

    Revive the project and delete the outdated scripts.

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import stock_attributes
 
+# if __name__ == '__main__':
 
-if __name__ == '__main__':
-    ticker_list = ['1475.HK', 'AAPL']
-    for s in ticker_list:
-        stock = stock_attributes.Stock(s)
-        print(stock.market_cap)
--- scrap_data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .scrap_yahoo import *
