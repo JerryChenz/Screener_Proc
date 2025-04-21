794dbec37772bf1c4429121060188513b89a7b5c
jerry <jerrychen.works@gmail.com>
2025-04-21 10:11:08 +0800

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import scrape_data
-import clean_data
-import screen_data
-
 """
 Financial Metrics:
 EBIT/P: Measures earnings yield.
@@ -23,14 +19,51 @@ Output:
 Results are saved to data/screened_data with region-specific files (e.g., us_screened.csv).
 """
 
+import scrape_data
+import clean_data
+import screen_data
+import json
+
+
+def json_to_list(json_file):
+    """
+    Read a JSON file and convert its contents into a Python list.
+
+    Args:
+        json_file (str): Path to the JSON file.
+
+    Returns:
+        list: A list of tickers.
+
+    Raises:
+        FileNotFoundError: If the JSON file does not exist.
+        json.JSONDecodeError: If the JSON file is invalid or cannot be decoded.
+    """
+
+    try:
+        with open(json_file, 'r') as file:
+            ticker_list = json.load(file)
+        return ticker_list
+
+    except FileNotFoundError:
+        print(f"Error: The file '{json_file}' does not exist.")
+        return []
+
+    except json.JSONDecodeError:
+        print(f"Error: The file '{json_file}' contains invalid JSON.")
+        return []
+
+
 if __name__ == "__main__":
+    json_file_path = 'data/ticker_library/hk_unique_tickers.json'
+
     # Step 1: Example list of tickers
-    # ticker_list = ['AAPL', 'MSFT', 'GOOGL', 'KHC', 'SIRI']
-    # csv_name = 'us'
-    # scrape_data.scrape_yfinance(ticker_list, csv_name)
+    tickers_list = json_to_list(json_file_path)
+    csv_name = 'us'
+    scrape_data.scrape_yfinance(tickers_list, csv_name)
 
     # Step 2: Clean Data
-    clean_data.clean_scraped_data()
+    # clean_data.clean_scraped_data()
 
     # Step 3: Screen
-    screen_data.screen_companies()
+    # screen_data.screen_companies()
